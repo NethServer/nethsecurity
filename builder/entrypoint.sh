e845f7719c5f0615c70be5b80d5b94fc391a63c6
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2022-04-29 11:38:25 +0200

    Support rootless containers

--- builder/entrypoint.sh
+++ builder/entrypoint.sh
@@ -2,14 +2,15 @@
 
 OUTPUT=/home/build/openwrt/.config
 
+# Fix permissions
+sudo chown -R build:build /config /home/build/openwrt/files/etc/uci-defaults /home/build/openwrt/bin
+
 # Generate diffconfigfrom .conf file inside config directory
 > $OUTPUT
 for f in $(find /config -type f -name \*.conf)
 do
     cat $f >> $OUTPUT
 done
-
-chown 1000:1000 $OUTPUT
 
 # Apply the configuration
 make defconfig
