63a2c56600296d185d241dbdd0bde45d8092610c
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2022-04-29 14:38:54 +0200

    run: fix selinux labels for rootless

--- builder/entrypoint.sh
+++ builder/entrypoint.sh
@@ -2,12 +2,16 @@
 
 OUTPUT=/home/build/openwrt/.config
 
+# hack to avoid changing permissions on local file system
+sudo cp -r /config /config-tmp
+sudo cp -r /uci-defaults/* /home/build/openwrt/files/etc/uci-defaults/
+
 # Fix permissions
-sudo chown -R build:build /config /home/build/openwrt/files/etc/uci-defaults /home/build/openwrt/bin
+sudo chown -R build:build /config-tmp /home/build/openwrt/files/etc/uci-defaults
 
 # Generate diffconfigfrom .conf file inside config directory
 > $OUTPUT
-for f in $(find /config -type f -name \*.conf)
+for f in $(find /config-tmp -type f -name \*.conf)
 do
     cat $f >> $OUTPUT
 done
