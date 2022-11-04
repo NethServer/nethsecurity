e5c2a02f936b222117efb72f81a99e1ba6681dd7
Nick <87082120+NethNick@users.noreply.github.com>
2022-11-04 10:32:08 +0100

    Multiple choice

--- files/usr/sbin/install-nx.sh
+++ files/usr/sbin/install-nx.sh
@@ -3,11 +3,11 @@ if [ $# -eq 0 ]; then
     echo -e "No arguments supplied, target device for installation needed\n$0 -t /dev/sdX [-s source]"    
     exit 1
 fi
-while getopts "ft::" opt; do
+while getopts "ft::s::" opt; do
             case $opt in
-            (f) F=1 ;;
-            (s) S=${OPTARG} ;;
-            (t) T=${OPTARG} ;;
+            (f) F=1 ;; #Force write
+            (s) S=${OPTARG} ;; #Source image
+            (t) T=${OPTARG} ;; #Target disk
             (*) printf "Illegal option '-%s'\n" "$opt" && exit 1 ;;
             esac
 done
@@ -18,16 +18,28 @@ if [ -b $T ]; then
            N=$(grep "${T##*/}" /proc/partitions | wc -l)
         fi
         M=$(mount | grep $T| wc -l)
-        S=$(df -t vfat /boot | tail -n 1| cut -d " " -f 1| tr "1" "3")
+        P=$(df -t vfat /boot | tail -n 1| cut -d " " -f 1| tr "1" "3")
         if [ $N -eq 1 ] && [ $M -eq 0 ]; then
-           mkdir mymount
-           mount -t vfat $S mymount
-           IMG=$(find mymount -name nextsecurity.img.gz| tail -n 1);
-           if [ -z ${S+x} ]; then IMG=$S fi
+           mkdir firmware
+           mount -t vfat $P firmware
+           I=$(find firmware -name nextsecurity\*img.gz| wc -l);
+           if [ "$I" -eq 1 ]; then
+              IMG=$(find firmware -name nextsecurity\*img.gz| tail -n 1);
+           else if [ -z ${S+x} ]; then 
+              IMG=$S 
+           else
+              let A=1; B=("");
+              echo "Choose one of the detected images to install to device:"
+              for I in $(ls -1f firmware/nextsecurity*img.gz); do 
+                 echo "$A. $I"; ((A+=1));B+=($I); 
+              done;
+              read -r $IMG
+              IMG=${B[$IMG]}
+           fi
            if [ ! -f $IMG ]; then
               echo "Firmware not found"
-              umount mymount
-              rmdir mymount
+              umount firmware
+              rmdir firmware
               exit 1
            else
               zcat $IMG| dd of=$T conv=notrunc
