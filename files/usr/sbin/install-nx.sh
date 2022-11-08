951a58b4d25529cae1b93edf2c7b5793131d425e
Nicola Filippini <nicola.filippini@nethesis.it>
2022-11-08 11:09:59 +0100

    install-nx : improved options handling

--- files/usr/sbin/install-nx.sh
+++ files/usr/sbin/install-nx.sh
@@ -3,11 +3,11 @@ if [ $# -eq 0 ]; then
     echo -e "No arguments supplied, target device for installation needed\n$0 -t /dev/sdX [-s source]"    
     exit 1
 fi
-while getopts "ft::" opt; do
+while getopts "ft::s::" opt; do
             case $opt in
-            (f) F=1 ;;
-            (s) S=${OPTARG} ;;
-            (t) T=${OPTARG} ;;
+            (f) F=1 ;; #Force write
+            (s) S=${OPTARG} ;; #Source image
+            (t) T=${OPTARG} ;; #Target disk
             (*) printf "Illegal option '-%s'\n" "$opt" && exit 1 ;;
             esac
 done
@@ -18,24 +18,42 @@ if [ -b $T ]; then
            N=$(grep "${T##*/}" /proc/partitions | wc -l)
         fi
         M=$(mount | grep $T| wc -l)
-        S=$(df -t vfat /boot | tail -n 1| cut -d " " -f 1| tr "1" "3")
+        P=$(df -t vfat /boot | tail -n 1| cut -d " " -f 1| tr "1" "3")
         if [ $N -eq 1 ] && [ $M -eq 0 ]; then
-           mkdir mymount
-           mount -t vfat $S mymount
-           IMG=$(find mymount -name nextsecurity.img.gz| tail -n 1);
-           if [ -z ${S+x} ]; then IMG=$S fi
+           mkdir /tmp/firmware
+           mount -t vfat $P /tmp/firmware
+           FW=( $(find /tmp/firmware -name nextsecurity\*img.gz| tr " " "$")) ;
+           if [ "${#FW[@]}" -eq 1 ]; then
+              IMG=${FW//$/ };
+           elif [ ! -z ${S+x} ]; then 
+              IMG="$S" 
+           else
+              let A=1; B=("");
+              echo "Choose one of the detected images to install to device:"
+              for I in ${FW[@]}; do
+                 I=${I//$/ };
+                 echo "$A. ${I:14}"; ((A+=1));B+=($I); 
+              done;
+              echo -n "Your choice: "
+              read -r IMG
+              IMG=${B[$IMG]}
+           fi
            if [ ! -f $IMG ]; then
               echo "Firmware not found"
-              umount mymount
-              rmdir mymount
+              umount /tmp/firmware
+              rmdir /tmp/firmware
               exit 1
            else
               zcat $IMG| dd of=$T conv=notrunc
            fi
-           umount mymount
-           rmdir mymount
+           umount /tmp/firmware
+           rmdir /tmp/firmware
         else
-           echo -e "Multiple partitions find on target device, check it or use -f to force overwrite"
+           if [ $M -eq 0 ]; then
+              echo -e "Multiple partitions find on target device, check it or use -f to force overwrite"
+           else
+              echo -e "Target partition in use, umount it first"
+           fi
         fi
 else
         echo -e "Target device not found"
