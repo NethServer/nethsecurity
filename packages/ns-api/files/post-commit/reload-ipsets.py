26b70dff2f44699366629760d88ddb649db82519
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2024-04-16 16:44:23 +0200

    ns-api: force ipset reload (#425)
    The 'fw4 reload' command does not update ipsets if an
    element has been removed.
    
    The new post-commit hook forces the reload of all ipsets
    if an ipset has been changed during last uci commit.

---
+++ packages/ns-api/files/post-commit/reload-ipsets.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+#
+# Copyright (C) 2024 Nethesis S.r.l.
+# SPDX-License-Identifier: GPL-2.0-only
+#
+
+# This script forces ipset reload: they are not correctly reloaded by fw4 reload
+
+import subprocess
+
+force_reload = False
+
+if 'firewall' in changes:
+    for change in changes['firewall']:
+        if 'ipset' in change:
+            force_reload = True
+            break
+
+if force_reload:
+    subprocess.run(["/sbin/fw4", "reload-sets"])
