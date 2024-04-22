47d188684bf7f912372fefd238b5ff4b53946179
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2024-04-22 10:44:10 +0200

    tools: cleanup, remove stale images from rolling repo

--- tools/cleanup/cleanup.py
+++ tools/cleanup/cleanup.py
@@ -68,3 +68,68 @@ for d in to_delete:
     delete_keys = {'Objects' : []}
     delete_keys['Objects'] = [{'Key' : k} for k in [obj['Key'] for obj in objects_to_delete.get('Contents', [])] ]
     s3_client.delete_objects(Bucket=bucket_name, Delete=delete_keys)
+
+# Cleanup image targets from rolling repository
+images = []
+bins = []
+manifests = []
+builders = []
+response = s3_client.list_objects_v2(Bucket=bucket_name, Prefix=f'{prefix}/', Delimiter='/')
+for o in response.get('CommonPrefixes'):
+    version = o.get('Prefix').removeprefix(f'{prefix}/').rstrip('/')
+    if version.startswith('8-'):
+        continue
+    # match only elements where prefix is like 'dev/23.05.2/'
+    if re.match(r'^(\d+\.\d+\.\d+)$', version):
+        target_dir = f'{prefix}/{version}/targets/x86/64/'
+        for i in s3_client.list_objects_v2(Bucket=bucket_name, Prefix=target_dir).get('Contents'):
+            # calculate basename of key
+            key = i['Key'].removeprefix(target_dir)
+            if re.match(rf'^nethsecurity-8-.*\.gz$', key):
+                images.append((i['Key'], i['LastModified']))
+            elif re.match(rf'^nethsecurity-8-.*\.bin$', key):
+                bins.append((i['Key'], i['LastModified']))
+            elif re.match(rf'^nethsecurity-8-.*\.manifest$', key):
+                manifests.append((i['Key'], i['LastModified']))
+            elif re.match(rf'^nethsecurity-imagebuilder-8-.*\.tar\.xz$', key):
+                builders.append((i['Key'], i['LastModified']))
+
+# sort images by date, more recent first
+images.sort(key=lambda x: x[1], reverse=True)
+# keep only 2 latest images (rootfs + combined)
+image_to_keep = images[:4]
+for image in images:
+    # delete image if not in image_to_keep
+    if image not in image_to_keep:
+        print(f"Deleting {image[0]} ...")
+        s3_client.delete_object(Bucket=bucket_name, Key=image[0])
+
+# sort bins by date, more recent first
+bins.sort(key=lambda x: x[1], reverse=True)
+# keep only 2 latest bins
+bins_to_keep = bins[:2]
+for bin in bins:
+    # delete bin if not in bins_to_keep
+    if bin not in bins_to_keep:
+        print(f"Deleting {bin[0]} ...")
+        s3_client.delete_object(Bucket=bucket_name, Key=bin[0])
+
+# sort manifests by date, more recent first
+manifests.sort(key=lambda x: x[1], reverse=True)
+# keep only 2 latest manifests
+manifests_to_keep = manifests[:2]
+for manifest in manifests:
+    # delete manifest if not in manifests_to_keep
+    if manifest not in manifests_to_keep:
+        print(f"Deleting {manifest[0]} ...")
+        s3_client.delete_object(Bucket=bucket_name, Key=manifest[0])
+
+# sort builders by date, more recent first
+builders.sort(key=lambda x: x[1], reverse=True)
+# keep only 2 latest builders
+builders_to_keep = builders[:2]
+for builder in builders:
+    # delete builder if not in builders_to_keep
+    if builder not in builders_to_keep:
+        print(f"Deleting {builder[0]} ...")
+        s3_client.delete_object(Bucket=bucket_name, Key=builder[0])
