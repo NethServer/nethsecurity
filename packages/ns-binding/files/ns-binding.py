b217ffcf018e7ef911cf18580406f75263299ef2
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2024-12-05 16:23:59 +0100

    fix: loosen control

--- packages/ns-binding/files/ns-binding.py
+++ packages/ns-binding/files/ns-binding.py
@@ -40,6 +40,24 @@ if len(dhcp_interfaces) > 0:
 
     table inet ns-binding {{
     
+        set ipV4Bound {{
+            type ipv4_addr
+            flags interval
+            auto-merge
+            {'elements = {' if len(reservations) > 0 else ''}
+                {', '.join(reservations.values())}
+            {'}' if len(reservations) > 0 else ''}
+        }}
+        
+        set etherBound {{
+            type ether_addr
+            flags interval
+            auto-merge
+            {'elements = {' if len(reservations) > 0 else ''}
+                {', '.join(reservations.keys())}
+            {'}' if len(reservations) > 0 else ''}
+        }}
+    
         set bindingListV4 {{
             type ether_addr . ipv4_addr
             policy memory
@@ -52,20 +70,24 @@ if len(dhcp_interfaces) > 0:
         
         # if interface is not in one where the DHCP server is configured, allow DHCP queries and check with bindingListV4 for all rest
         chain input {{
-            type filter hook input priority -110; policy drop;
+            type filter hook input priority -110; policy accept;
             ct state established,related accept
             iifname != {{ {' , '.join(dhcp_interfaces)} }} accept
             udp dport 67-68 accept
-            ether saddr . ip saddr @bindingListV4 counter accept
-            log flags all prefix "ns-binding DROP: " counter
+            ether saddr @etherBound counter jump binding
+            ip saddr @ipV4Bound counter jump binding
         }}
         
         chain forward {{
-            type filter hook forward priority -110; policy drop;
+            type filter hook forward priority -110; policy accept;
             ct state established,related accept
             iifname != {{ {' , '.join(dhcp_interfaces)} }} accept
-            ether saddr . ip saddr @bindingListV4 counter accept
-            log flags all prefix "ns-binding DROP: " counter
+            ether saddr @etherBound counter jump binding
+            ip saddr @ipV4Bound counter jump binding
+        }}
+        
+        chain binding {{
+            ether saddr . ip saddr != @bindingListV4 log flags all prefix "ns-binding DROP: " drop
         }}
     }}
     """
