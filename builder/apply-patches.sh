ac248187a8085284e24de3ffc0c48cff202d3d8e
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2025-04-08 14:47:16 +0200

    wip: first attempt at reproducible builds

---
+++ builder/apply-patches.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+#
+# Copyright (C) 2025 Nethesis S.r.l.
+# SPDX-License-Identifier: GPL-2.0-only
+#
+
+find patches/ -type f -name "*.patch" | while read -r patch; do
+    dir_name=$(dirname "$patch")
+    dir_name=${dir_name#"patches/"}
+    patch -d "$dir_name" -F 2 -p 1 < "$patch"
+done
