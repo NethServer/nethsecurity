afdfc1d2fd0cb334aa32f3fa20a68e15727c0af2
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2025-04-23 12:32:14 +0200

    chore(ns-ha): rename main to primary

--- packages/ns-ha/files/99-ns-ha.sh
+++ packages/ns-ha/files/99-ns-ha.sh
@@ -5,5 +5,5 @@ state=$(grep state /tmp/keepalived.conf 2>/dev/null | awk '{print $2}')
 if [ "$state" == "MASTER" ]; then
     export PS1='\u@\h [P]:\w\$ '
 elif [ "$state" == "BACKUP" ]; then
-    export PS1='\u@\h [S]:\w\$ '
+    export PS1='\u@\h [B]:\w\$ '
 fi
