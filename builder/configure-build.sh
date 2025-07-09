3c61f8400665e8a1d6fe73b66e996d70493618da
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2025-07-09 11:00:16 +0200

    feat: added configuration file

--- builder/configure-build.sh
+++ builder/configure-build.sh
@@ -13,20 +13,6 @@ repo_channel=${REPO_CHANNEL:?Missing REPO_CHANNEL environment variable}
 target=${TARGET:?Missing TARGET environment variable}
 owrt_version=${OWRT_VERSION:?Missing OWRT_VERSION environment variable}
 
-# Configure repositories
-sed -i '/telephony/d' feeds.conf.default
-sed -i 's/src-git-full/src-git/' feeds.conf.default
-sed -i '1isrc-link nethsecurity /home/buildbot/openwrt/nspackages' feeds.conf.default
-./scripts/feeds update
-./scripts/feeds install -a
-
-# Apply patches
-find patches/ -type f -name "*.patch" | while read -r patch; do
-    dir_name=$(dirname "$patch")
-    dir_name=${dir_name#"patches/"}
-    patch -d "$dir_name" -F 2 -p 1 < "$patch"
-done
-
 # Conclude configuration
 cat <<EOF >> config/.diffconfig
 CONFIG_GRUB_TITLE="NethSecurity"
@@ -43,17 +29,12 @@ CONFIG_VERSION_SUPPORT_URL="https://community.nethserver.org"
 EOF
 cat "config/targets/${target}.conf" >> config/.diffconfig
 
-# Write version information into a file
-echo "${repo_channel}" > files/etc/repo-channel
-
 # Netifyd closed-sources plugin
-if [ -z "$NETIFYD_ACCESS_TOKEN" ]; then
+if [ "$NETIFYD_ENABLED" -eq "0" ]; then
     echo "Netifyd closed-sources plugin not enabled: skipping ns-dpi package"
     echo CONFIG_PACKAGE_ns-dpi=n >> config/.diffconfig
 else
     echo "Netifyd closed-sources plugin enabled: enabling ns-dpi package"
-    git clone "https://oauth2:$NETIFYD_ACCESS_TOKEN@gitlab.com/netify.ai/private/nethesis/netify-flow-actions.git"
-    git clone "https://oauth2:$NETIFYD_ACCESS_TOKEN@gitlab.com/netify.ai/private/nethesis/netify-agent-stats-plugin.git"
     cat << EOF >> config/.diffconfig
 CONFIG_PACKAGE_netify-flow-actions=y
 CONFIG_NETIFY_FLOW_ACTIONS_TARGET_LOG=y
@@ -63,18 +44,9 @@ CONFIG_PACKAGE_netify-plugin-stats=y
 EOF
 fi
 
+# Write version information into a file
+echo "${repo_channel}" > files/etc/repo-channel
+
 # Expand configuration
 cp config/.diffconfig .config
 make defconfig
-
-# Generate local build key
-if [ -n "$USIGN_PUB_KEY" ] && [ -n "$USIGN_PRIV_KEY" ]; then
-    echo "Using environment keys"
-    echo "$USIGN_PUB_KEY" > ./key-build.pub
-    echo "$USIGN_PRIV_KEY" > ./key-build
-elif [ -f key-build.pub ] && [ -f key-build ]; then
-    echo "Using existing keys to sign the build"
-else
-    echo "Generating local build key"
-    ./staging_dir/host/bin/usign -G -s ./key-build -p ./key-build.pub -c "Local build key"
-fi
--- builder/entrypoint.sh
+++ builder/entrypoint.sh
@@ -7,6 +7,26 @@
 
 set -e
 
-setup-build
+if [ ! "$NETIFYD_ENABLED" -eq "0" ]; then
+    echo "Netifyd is enabled, downloading sources..."
+    git clone "https://oauth2:$NETIFYD_ACCESS_TOKEN@gitlab.com/netify.ai/private/nethesis/netify-flow-actions.git"
+    git clone "https://oauth2:$NETIFYD_ACCESS_TOKEN@gitlab.com/netify.ai/private/nethesis/netify-agent-stats-plugin.git"
+fi
 
-exec "$@"
+# if command $1 is a file or a executable, run it
+if [ -f "$1" ] && [ -x "$1" ]; then
+    exec "$@"
+else
+    # Otherwise, assume it's a make command and run it 
+    if [ -n "$USIGN_PUB_KEY" ] && [ -n "$USIGN_PRIV_KEY" ]; then
+        echo "$USIGN_PUB_KEY" > /home/buildbot/openwrt/key-build.pub
+        echo "$USIGN_PRIV_KEY" > /home/buildbot/openwrt/key-build
+    elif [ ! -f key-build.pub ] && [ ! -f key-build ] && [ -f ./staging_dir/host/bin/usign ]; then
+        ./staging_dir/host/bin/usign -G -s ./key-build -p ./key-build.pub -c "Local build key"
+    else 
+        echo "No signing keys found. Please provide USIGN_PUB_KEY and USIGN_PRIV_KEY environment variables or place key-build.pub and key-build in the current directory."
+        exit 1
+    fi
+
+    exec make -j"$(nproc)" "$@"
+fi
