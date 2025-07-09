f5f317c0dd2b22072b3b5eb15f094a27ae52c1c5
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2025-07-09 11:23:47 +0200

    skipping previous approach

--- build-nethsec.sh
+++ build-nethsec.sh
@@ -7,6 +7,27 @@
 
 set -e
 
+# Source build files if it exists
+set -o allexport
+if [ -f build.conf ]; then
+    echo "Loading build.conf file..."
+    . ./build.conf
+fi
+set +o allexport
+
+# Check required environment variables, override .env variables if set
+OWRT_VERSION=${OWRT_VERSION:?Missing OWRT_VERSION environment variable}
+NETHSECURITY_VERSION=${NETHSECURITY_VERSION:?Missing NETHSECURITY_VERSION environment variable}
+REPO_CHANNEL=${REPO_CHANNEL:-dev}
+TARGET=${TARGET:-x86_64}
+NETIFYD_ENABLED=${NETIFYD_ENABLED:-0}
+NETIFYD_ACCESS_TOKEN=${NETIFYD_ACCESS_TOKEN}
+
+if [ -z "$USIGN_PRIV_KEY" ] || [ -z "$USIGN_PUB_KEY" ]; then
+    USIGN_PRIV_KEY="$(cat ./build-sign-key)"
+    USIGN_PUB_KEY="$(cat ./build-sign-key.pub)"
+fi
+
 # Clean up previous builds
 rm -rf bin
 rm -rf build-logs
@@ -16,32 +37,33 @@ podman build \
     --layers \
     --file builder/Containerfile \
     --tag nethsecurity-next \
-    --build-arg-file build.conf \
+    --build-arg OWRT_VERSION="$OWRT_VERSION" \
+    --build-arg REPO_CHANNEL="$REPO_CHANNEL" \
+    --build-arg TARGET="$TARGET" \
+    --build-arg NETIFYD_ENABLED="$NETIFYD_ENABLED" \
     .
 
 set +e
 
-if [ -f "key-build.pub" ] && [ -f "key-build" ]; then
-    USIGN_PUB_KEY=$(cat key-build.pub)
-    USIGN_PRIV_KEY=$(cat key-build)
-elif [ -n "$USIGN_PUB_KEY" ] && [ -n "$USIGN_PRIV_KEY" ]; then
-    echo "Using provided USIGN keys."
-fi
-
 status=0
 podman run \
+    --env OWRT_VERSION="$OWRT_VERSION" \
+    --env REPO_CHANNEL="$REPO_CHANNEL" \
+    --env NETHSECURITY_VERSION="$NETHSECURITY_VERSION" \
+    --env TARGET="$TARGET" \
+    --env USIGN_PRIV_KEY="$USIGN_PRIV_KEY" \
+    --env USIGN_PUB_KEY="$USIGN_PUB_KEY" \
+    --env NETIFYD_ENABLED="$NETIFYD_ENABLED" \
+    --env NETIFYD_ACCESS_TOKEN="$NETIFYD_ACCESS_TOKEN" \
     --name nethsecurity-builder \
     --interactive \
     --tty \
     --replace \
-    --env-file build.conf \
-    --env USIGN_PUB_KEY \
-    --env USIGN_PRIV_KEY \
     --volume nethsecurity_builder_build:/home/buildbot/openwrt/build_dir \
-    --volume nethsecurity_builder_cache:/home/buildbot/openwrt/.ccache \
-    --volume nethsecurity_builder_dl:/home/buildbot/openwrt/dl \
-    --volume nethsecurity_builder_downloads:/home/buildbot/openwrt/download \
     --volume nethsecurity_builder_staging:/home/buildbot/openwrt/staging_dir \
+    --volume nethsecurity_builder_cache:/home/buildbot/openwrt/.ccache \
+    --volume nethsecurity_builder_downloads:/home/buildbot/openwrt/download \
+    --volume nethsecurity_builder_dl:/home/buildbot/openwrt/dl \
     nethsecurity-next \
     "$@" || status=$?
 
