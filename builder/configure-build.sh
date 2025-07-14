65ccd55a89de18bfe578ffeca7918a43b3b725da
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2025-07-14 13:55:34 +0200

    refactor: restored old config

--- builder/configure-build.sh
+++ builder/configure-build.sh
@@ -13,8 +13,18 @@ repo_channel=${REPO_CHANNEL:?Missing REPO_CHANNEL environment variable}
 target=${TARGET:?Missing TARGET environment variable}
 owrt_version=${OWRT_VERSION:?Missing OWRT_VERSION environment variable}
 
+# For each file inside the config directory, cat the content into a .config file
+for file in config/*.conf; do
+    if [ -f "$file" ]; then
+        echo "Processing $file"
+        cat "$file" >> .config
+    else
+        echo "Skipping $file, not a regular file"
+    fi
+done
+
 # Conclude configuration
-cat <<EOF >> config/.diffconfig
+cat <<EOF >> .config
 CONFIG_GRUB_TITLE="NethSecurity"
 CONFIG_VERSION_BUG_URL="https://github.com/NethServer/nethsecurity/issues"
 CONFIG_VERSION_DIST="NethSecurity"
@@ -27,15 +37,15 @@ CONFIG_VERSION_PRODUCT="NethSecurity"
 CONFIG_VERSION_REPO="https://updates.nethsecurity.nethserver.org/${repo_channel}/${nethsecurity_version}"
 CONFIG_VERSION_SUPPORT_URL="https://community.nethserver.org"
 EOF
-cat "config/targets/${target}.conf" >> config/.diffconfig
+cat "config/targets/${target}.conf" >> .config
 
 # Netifyd closed-sources plugin
 if [ "$NETIFYD_ENABLED" -eq "0" ]; then
     echo "Netifyd closed-sources plugin not enabled: skipping ns-dpi package"
-    echo CONFIG_PACKAGE_ns-dpi=n >> config/.diffconfig
+    echo CONFIG_PACKAGE_ns-dpi=n >> .config
 else
     echo "Netifyd closed-sources plugin enabled: enabling ns-dpi package"
-    cat << EOF >> config/.diffconfig
+    cat << EOF >> .config
 CONFIG_PACKAGE_netify-flow-actions=y
 CONFIG_NETIFY_FLOW_ACTIONS_TARGET_LOG=y
 CONFIG_NETIFY_FLOW_ACTIONS_TARGET_CTLABEL=y
@@ -48,5 +58,4 @@ fi
 echo "${repo_channel}" > files/etc/repo-channel
 
 # Expand configuration
-cp config/.diffconfig .config
 make defconfig
