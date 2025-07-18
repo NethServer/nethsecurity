def8d6d8acf619658b05e04b7cd9c41ead864aa7
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2025-07-18 10:28:51 +0200

    fix(ns-api): clean up mwan configuration where fields are empty

--- packages/ns-api/files/pre-commit/clean-network.py
+++ packages/ns-api/files/pre-commit/clean-network.py
@@ -15,38 +15,72 @@ if 'network' in changes:
     for item in e_uci.get('qosify'):
         if e_uci.get('qosify', item) == 'interface':
             qosify_interfaces.append(item)
-    mwan_interfaces = []
-    for item in e_uci.get('mwan3'):
-        if e_uci.get('mwan3', item) == 'interface':
-            mwan_interfaces.append(item)
 
+    mwan3_entries = e_uci.get('mwan3')
+    mwan_interfaces = []
+    for entry in mwan3_entries:
+        if e_uci.get('mwan3', entry) == 'interface':
+            mwan_interfaces.append(entry)
+
+    qosify_changed = False
+    mwan_changed = False
     for item in changes['network']:
         # if something in network has been removed
         if len(item) > 0 and item[0] == 'remove':
             to_remove_interface = item[1]
             # if the interface is configured in qosify remove it
             if to_remove_interface in qosify_interfaces:
+                qosify_changed = True
                 e_uci.delete('qosify', to_remove_interface)
             # if the interface is configured in mwan3 remove the interface
             if to_remove_interface in mwan_interfaces:
+                # fetch all configuration
+                policies = []
+                rules = []
+                members = []
+                for entry in mwan3_entries:
+                    if e_uci.get('mwan3', entry) == 'policy':
+                        policies.append(entry)
+                    if e_uci.get('mwan3', entry) == 'rule':
+                        rules.append(entry)
+                    if e_uci.get('mwan3', entry) == 'member':
+                        members.append(entry)
+
+                # remove the interface from mwan3
+                mwan_changed = True
                 e_uci.delete('mwan3', to_remove_interface)
+
                 # iterate over the members of mwan3 and remove the associated member of the interface
                 members_removed = []
-                for entry in e_uci.get('mwan3'):
-                    if e_uci.get('mwan3', entry) == 'member':
-                        if to_remove_interface == e_uci.get('mwan3', entry, 'interface'):
-                            e_uci.delete('mwan3', entry)
-                            members_removed.append(entry)
+                for member in members:
+                    if e_uci.get('mwan3', member, 'interface') == to_remove_interface:
+                        e_uci.delete('mwan3', member)
+                        members_removed.append(member)
 
-                # iterate over the members removed and remove them from the policies
-                for entry in e_uci.get('mwan3'):
-                    if e_uci.get('mwan3', entry) == 'policy':
-                        members = list(e_uci.get('mwan3', entry, 'use_member', default=[], dtype=str, list=True))
-                        for member in members_removed:
-                            if member in members:
-                                members.remove(member)
-                        e_uci.set('mwan3', entry, 'use_member', members)
+                # iterate over the policies to see if they use the members removed
+                policies_removed = []
+                for policy in policies:
+                    policy_members = list(e_uci.get('mwan3', policy, 'use_member', default=[], dtype=str, list=True))
+                    for member in members_removed:
+                        if member in policy_members:
+                            policy_members.remove(member)
+                    if len(policy_members) == 0:
+                        e_uci.delete('mwan3', policy)
+                        policies_removed.append(policy)
 
+                # iterate over the rules to see if they use the policies removed
+                for rule in rules:
+                    rule_policies = list(e_uci.get('mwan3', rule, 'use_policy', default=[], dtype=str, list=True))
+                    for policy in policies_removed:
+                        if policy in rule_policies:
+                            rule_policies.remove(policy)
+                    if len(rule_policies) == 0:
+                        e_uci.delete('mwan3', rule)
 
-    e_uci.save('qosify')
-    e_uci.save('mwan3')
+    if qosify_changed:
+        e_uci.save('qosify')
+        changes['qosify'] = {}
+
+    if mwan_changed:
+        e_uci.save('mwan3')
+        changes['mwan3'] = {}
