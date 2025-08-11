e1ca2984edc6fe221a4734fbba7cd48ab551fa86
Filippo Carletti <filippo.carletti@gmail.com>
2025-08-11 15:51:40 +0200

    chore(base-files): Better TERM for serial console
    Use "vt102" for serial console terminal
    See https://github.com/openwrt/openwrt/commit/1310e4f1aecef6c2c1b68227216ba7cc04006002
    for details.

--- files/usr/libexec/login.sh
+++ files/usr/libexec/login.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
-[ "$(uci -q get system.@system[0].ttylogin)" = 1 ] || exec /bin/ash --login
+[ -t 0 ] && {
+	tty_dev=$(readlink /proc/self/fd/0)
+	case "$tty_dev" in
+		/dev/console|/dev/tty[0-9]*)
+			export TERM=${TERM:-linux}
+			;;
+		/dev/*)
+			export TERM=vt102
+			;;
+	esac
+}
+
+[ "$(uci -q get system.@system[0].ttylogin)" = 1 ] || exec /bin/login -f root
 
 [ -f /etc/keymap ] && /sbin/loadkmap < /usr/share/keymaps/$(/bin/cat /etc/keymap).map.bin 2>/dev/null
 
