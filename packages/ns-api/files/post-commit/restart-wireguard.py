126dc791f60a4b0a9a2d12dccdc4f39032ab5cfb
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2025-09-23 14:28:09 +0200

    added interface restart and client management

---
+++ packages/ns-api/files/post-commit/restart-wireguard.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+#
+# Copyright (C) 2025 Nethesis S.r.l.
+# SPDX-License-Identifier: GPL-2.0-only
+#
+
+# this script reloads wireguard interfaces if configuration has changed
+
+import subprocess
+from euci import EUci
+
+if 'network' in changes:
+    e_uci = EUci()
+    for item in e_uci.get('network'):
+        if e_uci.get('network', item, 'proto') == 'wireguard':
+            subprocess.call(f'ifdown {item} && ifup {item}', shell=True)
