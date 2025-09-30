536238a4525d703272783287e599cdb934fc54aa
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2025-09-30 11:48:49 +0200

    feat(wireguard): bringing wireguard to UI (#1370)

---
+++ packages/ns-api/files/post-commit/restart-wireguard.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+#
+# Copyright (C) 2025 Nethesis S.r.l.
+# SPDX-License-Identifier: GPL-2.0-only
+#
+
+# this script reloads wireguard interfaces if configuration has changed
+
+import subprocess
+
+if 'network' in changes:
+    interfaces_to_restart = []
+    for entry in changes['network']:
+        if entry[1].startswith('wg'):
+            interfaces_to_restart.append(entry[1][:3])
+    for item in set(interfaces_to_restart):
+        subprocess.call(f'ifdown {item} ; ifup {item}', shell=True)
