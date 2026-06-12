d697100f5929747895e49fd2dba58d66b6c1bb49
Matteo Di Lorenzi <52289654+m-dilorenzi@users.noreply.github.com>
2026-06-12 10:46:03 +0200

    feat(geoblocking): geo ip management in threat shield ip (#1714)

--- packages/ns-threat_shield/files/adjust-banip.py
+++ packages/ns-threat_shield/files/adjust-banip.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python
 
 #
-# Copyright (C) 2024 Nethesis S.r.l.
+# Copyright (C) 2026 Nethesis S.r.l.
 # SPDX-License-Identifier: GPL-2.0-only
 #
 
 # This script makes sure the status of banip service corresponds to the configured one
 
+import os
 import subprocess
 from euci import EUci
 
 # The changes variable is already within the scope from the caller
 if 'banip' in changes or 'network' in changes:
+
+    # if ban_country property was modified, remove stale backup files to prevent old country selections from being restored
+    if 'banip' in changes and any('ban_country' in op for op in changes['banip']):
+        for backup_file in ['/tmp/banIP-backup/banIP.countryv4.gz', '/tmp/banIP-backup/banIP.countryv6.gz']:
+            try:
+                os.remove(backup_file)
+            except FileNotFoundError:
+                pass
+    
     uci = EUci()
     enabled = uci.get('banip', 'global', 'ban_enabled', default='0')
     try:
