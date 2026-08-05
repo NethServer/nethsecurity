7dd83ac0550c9e49358032f3a088683be529ffc2
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2026-08-05 12:17:09 +0200

    build: escapes were needed for the docs, noted.
    This reverts commit 10b76388d907e577befbd6061e694c5fbe4355d4.

--- packages/python3-nethsec/src/nethsec/utils/__init__.py
+++ packages/python3-nethsec/src/nethsec/utils/__init__.py
@@ -24,7 +24,7 @@ def get_random_id():
     Random ids:
       - have a length of 11 characters
       - are sanitized accordingly to UCI conventions (see 'sanitize' function)
-      - start with ns_ prefix
+      - start with ns\_ prefix
 
     Arguments:
       - name -- the name of the section
@@ -44,7 +44,7 @@ def get_id(name, length = 100):
     All auto-generated NethSecurity ids:
 
       - have a maximum length of 100 characters
-      - start with ns_ prefix
+      - start with ns\_ prefix
       - are sanitized accordingly to UCI conventions
 
     Arguments:
